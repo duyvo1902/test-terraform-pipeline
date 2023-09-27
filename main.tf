1255dd3988363599018c958088f7964af980b322
Duy Vo <voqudu01@risk.regn.net>
2023-09-27 16:12:20 +0700

    Init pipeline

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"
+    }
+  }
+  backend "s3" {
+    bucket = "api-gateway-lambda-dynamodb" # change to name of your bucket
+    region = "us-west-1"                   # change to your region
+    key    = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://s3.localhost.localstack.cloud:4566"
+  }
+}
+
+resource "aws_dynamodb_table" "questions" {
+  name           = "questions"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
