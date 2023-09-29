8e23df360eaa30075df573eb64166979432df2a5
Duy Vo <voqudu01@risk.regn.net>
2023-09-29 17:50:48 +0700

    Update pipeline

--- provider.tf
+++ provider.tf
@@ -8,8 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
   region                      = "us-east-1"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
