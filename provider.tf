29f7d83cdf1857c95a5cfe486a88416275b0b147
Duy Vo <voqudu01@risk.regn.net>
2023-10-01 09:45:55 +0700

    Update pipeline

--- provider.tf
+++ provider.tf
@@ -8,7 +8,10 @@ terraform {
 }
 
 provider "aws" {
+  access_key                  = "test"
   region                      = "us-east-1"
+  s3_force_path_style         = true
+  secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
