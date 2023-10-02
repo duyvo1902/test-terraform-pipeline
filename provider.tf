1aed9aadf9589b9b42cb43b1fcbb8f414d8b2a6f
Duy Vo <voqudu01@risk.regn.net>
2023-10-02 09:31:44 +0700

    Update pipeline

--- provider.tf
+++ provider.tf
@@ -11,6 +11,7 @@ provider "aws" {
   access_key                  = "test"
   region                      = "us-east-1"
   secret_key                  = "test"
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
