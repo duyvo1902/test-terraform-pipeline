281ca93f8ea86caadab84b446e80cb0403004d08
Duy Vo <voqudu01@risk.regn.net>
2023-10-02 09:25:21 +0700

    Update pipeline

--- main.tf
+++ main.tf
@@ -1,18 +1,7 @@
-resource "aws_s3_bucket" "duy_terraform" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "duy" {
+  bucket = "duy-bucket-test-123"
   tags = {
-    Name       = "duy-bucket"
+    Name       = "My bucket"
     Enviroment = "Dev"
   }
-}
-
-resource "aws_s3_object" "object_www" {
-  depends_on   = [aws_s3_bucket.s3_bucket]
-  for_each     = fileset("${path.root}", "www/*.html")
-  bucket       = aws_s3_bucket.s3_bucket.id
-  key          = basename(each.value)
-  source       = each.value
-  etag         = filemd5("${each.value}")
-  content_type = "text/html"
-  acl          = "public-read"
 }
--- provider.tf
+++ provider.tf
@@ -10,7 +10,6 @@ terraform {
 provider "aws" {
   access_key                  = "test"
   region                      = "us-east-1"
-  s3_force_path_style         = true
   secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
