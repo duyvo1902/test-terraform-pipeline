9789de8fbb019e6d052254cfa7029f91c1438c18
Duy Vo <voqudu01@risk.regn.net>
2023-10-04 09:08:02 +0700

    Update pipeline

--- provider.tf
+++ provider.tf
@@ -10,13 +10,11 @@ terraform {
 provider "aws" {
   access_key                  = "test"
   region                      = "us-east-1"
-  secret_key                  = "test"
-  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
 
   endpoints {
-    s3 = "http://localhost:4566"
+    s3             = "http://s3.localhost.localstack.cloud:4566"
   }
 }
--- terragrunt.hcl
+++ terragrunt.hcl
@@ -2,12 +2,12 @@
 remote_state {
   backend = "s3"
   generate = {
-    path      = "main.tf"
+    path      = "backend.tf"
     if_exists = "overwrite_terragrunt"
   }
 
   config = {
-    bucket = "duy-bucket-test"
+    bucket = "my-bucket-test-03"
     key = "${path_relative_to_include()}/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
